66116440e673e2f4c799b7b13afc31d6a9ae29fa
taiki yamashita <taikity0808@gmail.com>
2021-08-25 20:22:16 +0900

    update: post controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,6 @@
 module Api 
     module V1 
-        class Postcontroller < Applicationcontroller
+        class PostsController < ApplicationController
             before_action :set_post, only: [:show, :update, :destroy]
 
             def index 
